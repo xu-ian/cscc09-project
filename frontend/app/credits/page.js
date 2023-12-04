c8eef4049056d658e7070260758a61eb761cf690
Ian Xu <ian.xu@mail.utoronto.ca>
2023-12-04 10:30:16 -0500

    Updated credits page to include grapesjs as a 3rd party package.

--- frontend/app/credits/page.js
+++ frontend/app/credits/page.js
@@ -11,10 +11,18 @@ function Credits(){
       <h2>SVG Images</h2>
       <ul>
         <li>
-          Icons created in SVG Path Editor
-          <a href="https://yqnn.github.io/svg-path-editor/" title="SVG Path Editor">https://yqnn.github.io/svg-path-editor/</a>
-          which is made by
-          <a href="https://github.com/Yqnn" title="YQNN's github page" target="_blank">YQNN</a>
+          Icons created in SVG Path Editor <a href="https://yqnn.github.io/svg-path-editor/" title="SVG Path Editor">
+            https://yqnn.github.io/svg-path-editor/
+          </a> which is made by <a href="https://github.com/Yqnn" title="YQNN's github page" target="_blank">
+            YQNN
+          </a>
+        </li>
+        <li>
+          Framework for website builder uses 3rd party package grapesJS <a href="https://grapesjs.com/" title="GrapesJS">
+            https://grapesjs.com/
+          </a> which is made by <a href="https://github.com/artf" title="Artur Arseniev's github page">
+              Artur Arseniev
+          </a>
         </li>
       </ul>
     </div>
